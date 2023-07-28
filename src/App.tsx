73798d3d9ef2ee7d637123e5997b6d198ea13014
woohyunjng <woohyun.jng08@gmail.com>
2023-07-29 02:00:19 +0900

    feat(pages/Main): Main page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { Component } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { ConfigProvider } from "antd";
+
 import Main from "./pages/Main.tsx";
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<BrowserRouter basename="/Turing-Machine">
-					<Routes>
-						<Route path="/" element={<Main />} />
-					</Routes>
-				</BrowserRouter>
+				<ConfigProvider
+					theme={{
+						token: {
+							fontFamily: "'Do Hyeon', sans-serif;",
+						},
+					}}
+				>
+					<BrowserRouter basename="/Turing-Machine">
+						<Routes>
+							<Route path="/" element={<Main />} />
+						</Routes>
+					</BrowserRouter>
+				</ConfigProvider>
 			</div>
 		);
 	}
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,55 @@
 import { Component } from "react";
-import { Button } from "antd";
+
+import { Typography, Button } from "antd";
 
 class Main extends Component {
 	render() {
-		return <Button>Hello World!</Button>;
+		return (
+			<div
+				style={{
+					textAlign: "center",
+					display: "block",
+				}}
+			>
+				<Typography.Title
+					style={{
+						marginTop: "30vh",
+					}}
+				>
+					튜링 머신
+				</Typography.Title>
+				<div>
+					<Button
+						style={{
+							marginTop: "0.5px",
+							width: "200px",
+						}}
+					>
+						새로 만들기
+					</Button>
+				</div>
+				<div>
+					<Button
+						style={{
+							marginTop: "0.5px",
+							width: "200px",
+						}}
+					>
+						불러오기
+					</Button>
+				</div>
+				<div>
+					<Button
+						style={{
+							marginTop: "0.5px",
+							width: "200px",
+						}}
+					>
+						도움말
+					</Button>
+				</div>
+			</div>
+		);
 	}
 }
 
