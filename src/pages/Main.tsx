99bb37a2cf9d48d4d75c31296551925916246ae4
woohyunjng <woohyun.jng08@gmail.com>
2023-07-31 03:13:44 +0900

    refactor(pages/Main): use StyledMainContainer

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,24 +1,34 @@
 import { useNavigate } from "react-router-dom";
 
-import { MainContainer, MainBtn, MainTitle } from "../components/styled.tsx";
+import {
+	StyledMainContainer,
+	StyledMainBtn,
+	StyledMainTitle,
+} from "../components/styled.tsx";
 
 const Main = () => {
 	const navigate = useNavigate();
 
 	return (
-		<MainContainer>
-			<MainTitle>튜링 머신</MainTitle>
+		<StyledMainContainer>
+			<StyledMainTitle>튜링 머신</StyledMainTitle>
 			<div>
-				<MainBtn onClick={() => navigate("/make")}>새로 만들기</MainBtn>
+				<StyledMainBtn onClick={() => navigate("/make")}>
+					새로 만들기
+				</StyledMainBtn>
 			</div>
 			<div>
-				<MainBtn onClick={() => navigate("/make")}>불러오기</MainBtn>
+				<StyledMainBtn onClick={() => navigate("/make")}>
+					불러오기
+				</StyledMainBtn>
 			</div>
 			<div>
-				<MainBtn onClick={() => navigate("/help")}>도움말</MainBtn>
+				<StyledMainBtn onClick={() => navigate("/help")}>
+					도움말
+				</StyledMainBtn>
 			</div>
 			<div>
-				<MainBtn
+				<StyledMainBtn
 					onClick={() =>
 						window.location.replace(
 							"https://github.com/woohyunjng/Turing-Machine"
@@ -26,9 +36,9 @@ const Main = () => {
 					}
 				>
 					깃허브
-				</MainBtn>
+				</StyledMainBtn>
 			</div>
-		</MainContainer>
+		</StyledMainContainer>
 	);
 };
 
