9fffba5e092e9d13ea10103169f2fe9b1fc4540c
woohyunjng <woohyun.jng08@gmail.com>
2023-07-31 03:28:33 +0900

    feat(pages/Main): support responsive

--- src/components/styled.tsx
+++ src/components/styled.tsx
@@ -8,11 +8,70 @@ export const StyledMainContainer = styled.div`
 
 export const StyledMainTitle = styled(Typography.Title)`
 	margin-top: 25vh;
+
+	@media only screen and (min-width: 2560px) {
+		font-size: 150px !important;
+	}
+
+	@media only screen and (min-width: 1920px) and (max-width: 2559px) {
+		font-size: 100px !important;
+	}
+
+	@media only screen and (min-width: 1000px) and (max-width: 1919px) {
+		font-size: 60px !important;
+	}
+
+	@media only screen and (min-width: 640px) and (max-width: 999px) {
+		font-size: 40px !important;
+	}
+
+	@media only screen and (max-width: 639px) {
+		font-size: 30px !important;
+	}
 `;
 
 export const StyledMainBtn = styled(Button)`
-	margin-top: 0.5px;
-	width: 200px;
+	@media only screen and (min-width: 2560px) {
+		width: 1000px;
+		margin-top: 30px;
+		height: 70px;
+		font-size: 40px;
+	}
+
+	@media only screen and (min-width: 1920px) and (max-width: 2559px) {
+		width: 800px;
+		margin-top: 20px;
+		height: 50px;
+		font-size: 30px;
+	}
+
+	@media only screen and (min-width: 1000px) and (max-width: 1919px) {
+		width: 500px;
+		margin-top: 5px;
+		height: 40px;
+		font-size: 20px;
+	}
+
+	@media only screen and (min-width: 640px) and (max-width: 999px) {
+		width: 400px;
+		margin-top: 5px;
+		height: 30px;
+		font-size: 15px;
+	}
+
+	@media only screen and (min-width: 320px) and (max-width: 639px) {
+		width: 200px;
+		margin-top: 5px;
+		height: 30px;
+		font-size: 15px;
+	}
+
+	@media only screen and (max-width: 319px) {
+		width: 150px;
+		margin-top: 5px;
+		height: 30px;
+		font-size: 15px;
+	}
 `;
 
 export const StyledBlankContainer = styled.div`
